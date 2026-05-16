a7a7db532e82dada6525bba0105e443665aa2362
Amadhi Wijesinghe <amadhikaveendaya@gmail.com>
2026-05-17 00:53:47 +0530

    Use Brevo SMTP

--- Backend/server.js
+++ Backend/server.js
@@ -523,23 +523,23 @@ app.get("/backup-db", async (req, res) => {
 
     console.log("STEP 4 - mysqldump completed");
 
-const transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
 
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
 
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
     console.log("STEP 5 - Sending email");
 
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
+      from: "nirmalaniplantation@gmail.com",
+      to: "nirmalaniplantation@gmail.com",
 
       subject: "Monthly Payroll Backup",
 
