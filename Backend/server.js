e8162633a741c21e57504f29cdb9be6bb96e2277
Amadhi Wijesinghe <amadhikaveendaya@gmail.com>
2026-06-09 00:52:03 +0530

    file upload

--- Backend/server.js
+++ Backend/server.js
@@ -19,9 +19,23 @@ app.use(cors({
 
 app.use(express.json());
 
+const uploadDir = path.join(
+  __dirname,
+  "uploads",
+  "expenditure"
+);
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, {
+    recursive: true,
+  });
+}
+
+console.log("Upload Directory:", uploadDir);
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/expenditure");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
